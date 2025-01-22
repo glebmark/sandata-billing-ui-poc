35e08d9f7ddfd96f4ad63f382f34c10d049d4ea2
glebmark <87950237+glebmark@users.noreply.github.com>
2025-01-22 21:37:33 +0700

    view and rate pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { StartPageComponent } from './start-page/start-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StartPageComponent],
   template: `<app-start-page />`,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [StartPageComponent],
 })
 export class AppComponent {
   title = 'sandata-billing-ui-poc';
--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-start-page',
-  templateUrl: './start-page.component.html',
+  template: `<div>
+    <h1>Welcome to the Sandata Billing UI</h1>
+    <nav>
+      <ul>
+        <li><a routerLink="/views">Views Page</a></li>
+        <li><a routerLink="/rate">Rate Page</a></li>
+      </ul>
+    </nav>
+    <router-outlet />
+  </div>
+`,
+imports: [RouterLink, RouterOutlet],
 //   styleUrls: ['./start-page.component.css']
 })
 export class StartPageComponent {}
