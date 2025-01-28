cf2d3cdaf48e8bdc7d46579e93c562e6efa1f11d
glebmark <87950237+glebmark@users.noreply.github.com>
2025-01-28 14:23:16 +0700

    dynamic rate editor

--- src/app/rate-page/rate-dynamic-subeditor.component.ts
+++ src/app/rate-page/rate-dynamic-subeditor.component.ts
@@ -2,14 +2,26 @@ import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
 
+// TODO implement formula of
+// dynamic rate calculation
 @Component({
     selector: "app-rate-dynamic-subeditor",
     template: `
     <form [formGroup]="rateForm">
         <div class="form-row">
             <mat-form-field>
+                <mat-label>Units</mat-label>
+                <input matInput formControlName="units" />
+            </mat-form-field>
+            units per
+            <mat-form-field>
                 <mat-label>Rate</mat-label>
                 <input matInput formControlName="rate" placeholder="$" />
+            </mat-form-field>
+            with modifier
+            <mat-form-field>
+                <mat-label>Modifier</mat-label>
+                <input matInput formControlName="modifier" />
             </mat-form-field>
         </div>
     </form>
@@ -20,11 +32,13 @@ import { MatInputModule } from "@angular/material/input";
         ],
     })
 export class RateDynamicSubeditorComponent {
-    rateForm: FormGroup;
+      rateForm: FormGroup;
     
       constructor(private fb: FormBuilder) {
         this.rateForm = this.fb.group({
+          units: [''],
           rate: [''],
+          modifier: [''],
         });
-      }
+    }
 }
